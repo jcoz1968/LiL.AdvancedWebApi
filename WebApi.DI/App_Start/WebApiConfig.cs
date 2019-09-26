cd4fa56e1f3823845d24b5cb67838cb78074e195
Jeffery Cosby <jcoz1968@gmail.com>
2019-09-25 22:16:52 -0500

    query string versioning

--- WebApi.DI/App_Start/WebApiConfig.cs
+++ WebApi.DI/App_Start/WebApiConfig.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 using Unity;
 using Unity.Lifetime;
+using WebApi.DI.Custom;
 using WebApi.DI.Data;
 using WebApi.DI.Data.DependencyResolver;
 
@@ -27,27 +29,29 @@ namespace WebApi.DI
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
+            config.Services.Replace(typeof(IHttpControllerSelector), new CustomControllerSelector(config));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
 
             // JSON Formatter
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
-            config.Routes.MapHttpRoute(
-                name: "StudentsV1",
-                routeTemplate: "api/v1/students/{id}",
-                defaults: new { controller="StudentsV1", id = RouteParameter.Optional }
-            );
+            //config.Routes.MapHttpRoute(
+            //    name: "StudentsV1",
+            //    routeTemplate: "api/v1/students/{id}",
+            //    defaults: new { controller="StudentsV1", id = RouteParameter.Optional }
+            //);
 
-            config.Routes.MapHttpRoute(
-                name: "StudentsV2",
-                routeTemplate: "api/v2/students/{id}",
-                defaults: new { controller = "StudentsV2", id = RouteParameter.Optional }
-            );
+            //config.Routes.MapHttpRoute(
+            //    name: "StudentsV2",
+            //    routeTemplate: "api/v2/students/{id}",
+            //    defaults: new { controller = "StudentsV2", id = RouteParameter.Optional }
+            //);
         }
     }
 }
