5f73fd3334095912c9b96bb52e3ed87c589f6e49
Jeffery Cosby <jcoz1968@gmail.com>
2019-09-25 20:17:39 -0500

    add unity

---
+++ WebApi.DI/Data/DependencyResolver/UnityResolver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using Unity;
+
+namespace WebApi.DI.Data.DependencyResolver
+{
+    public class UnityResolver : IDependencyResolver
+    {
+        protected IUnityContainer container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            this.container = container ?? throw new ArgumentNullException("container is null");
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+               
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+
+            }
+        }
+    }
+}
