3abfc44c854eae4f00d3a49ce2d905af016a7177
Jeffery Cosby <jcoz1968@gmail.com>
2019-09-25 23:14:13 -0500

    configure odata endpoint

---
+++ WebApi.OData/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.OData.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public AppDbContext() : base()
+        {
+
+        }
+    }
+}
---
+++ WebApi.OData/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.OData.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double GPA { get; set; }
+
+    }
+}
