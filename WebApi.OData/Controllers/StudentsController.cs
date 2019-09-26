765be44555a417c6a99697be661861c2646a6218
Jeffery Cosby <jcoz1968@gmail.com>
2019-09-26 18:56:18 -0500

    create student, get one

--- WebApi.OData/Controllers/StudentsController.cs
+++ WebApi.OData/Controllers/StudentsController.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
 using WebApi.OData.Models;
 
 namespace WebApi.OData.Controllers
@@ -15,6 +17,23 @@ namespace WebApi.OData.Controllers
         public IQueryable<Student> Get()
         {
             return db.Students;
+        }
+
+        [EnableQuery]
+        public SingleResult<Student> Get([FromODataUri]int key)
+        {
+            IQueryable<Student> result = db.Students.Where(x => x.Id == key);
+            return SingleResult.Create(result);
+        }
+        public async Task<IHttpActionResult> Post(Student student) 
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Students.Add(student);
+            await db.SaveChangesAsync();
+            return Created(student);
         }
 
         private bool StudentExists(int key)
